e4589d486c7fe5e2681c7ba472cd23a4f0c91ab5
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-30 09:55:46 +0300

    Pharmacist

---
+++ StoreApp1/Pharmacist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreApp1
+{
+    public class Pharmacist
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string  Post { get; set; }
+        public int Age { get; set; }
+        public string Experience { get; set; }
+        public double Salary { get; set; }
+    }
+}
--- StoreApp1/Program.cs
+++ StoreApp1/Program.cs
@@ -10,25 +10,33 @@ namespace StoreApp1
     class Program 
     {
         public static List<Product> prod = new List<Product>();
+        public static List<Pharmacist> pharm = new List<Pharmacist>();
 
         static void Main(string[] args)
         {
             Console.WriteLine("Добро пожаловать в магазин игрушек!");
             Console.WriteLine("Выберите команду:");
-            Console.WriteLine("1 - Показать товары");
+            Console.WriteLine("1 - Показать товары и фармацевтов");
             Console.WriteLine("2 - Выйти");
             String str = Console.ReadLine();
             switch (str)
             {
                 case "1":
-                    Console.WriteLine("Список товаров:");
-                    prod.Add(new Product() { ID = 01, Name = "Пеленка", Description = "Одноразовые пеленки 60х90", Maker = "Helen Harper", Price = 687.70, Availaible = true, Count = 5 });
-                    prod.Add(new Product() { ID = 02, Name = "Памперсы", Description = "Японские памперсы 9-14кг", Maker = "Momi", Price = 999.99, Availaible = true, Count = 6 });
-                    prod.Add(new Product() { ID = 03, Name = "Коляска", Description = "Коляска-Трансформер 2 в 1", Maker = "LINDA", Price = 20010.05, Availaible = false, Count = 0 });
-                    prod.Add(new Product() { ID = 04, Name = "Бутылочка", Description = "Силиконовая бутылочка для кормления", Maker = "Olobaby", Price = 2745.50, Availaible = false, Count = 0 });
-                    prod.Add(new Product() { ID = 05, Name = "Детская смесь", Description = "Молочко детское", Maker = "Nestle", Price = 889.90, Availaible = false, Count = 0 });
+               
+                    prod.Add(new Product() { ID = 01, Name = "Банеоцин", Description = "Порошок для наружного применения 10г", Maker = "Монтавит", Price = 449.70, Availaible = true, Count = 5 });
+                    prod.Add(new Product() { ID = 02, Name = "Пенталгин", Description = "Таблетки покрыт.плен.об. 12 шт.", Maker = "Фармстандарт-Лексредства", Price = 119.99, Availaible = true, Count = 6 });
+                    prod.Add(new Product() { ID = 03, Name = "Фосфалюгель", Description = "Гель для приема внутрь 20 шт.", Maker = "Фарматис", Price = 489.05, Availaible = false, Count = 0 });
+                    prod.Add(new Product() { ID = 04, Name = "Либридерм", Description = "Серацин матирующий дневной крем 50 мл", Maker = "Дина+", Price = 409.50, Availaible = false, Count = 0 });
+                    prod.Add(new Product() { ID = 05, Name = "Риностоп", Description = "Капли назальные 0,1% 10 мл", Maker = "Лекко ЗАО", Price = 33.90, Availaible = false, Count = 0 });
+
+                    pharm.Add(new Pharmacist() { ID = 01, FirstName = "Влад", SurName = "Рыбачков",Post = "Младший фармацевт", Age = 20 , Experience = "1 год" , Salary = 20000});
+                    pharm.Add(new Pharmacist() { ID = 02, FirstName = "Булат", SurName = "Шакур", Post = "Старший фармацевт", Age = 25, Experience = "5 лет", Salary = 25000 });
+                    pharm.Add(new Pharmacist() { ID = 03, FirstName = "Талгат", SurName = "Яруллин", Post = "Стажер", Age = 18, Experience = "2 месяца", Salary = 13500 });
+                    pharm.Add(new Pharmacist() { ID = 04, FirstName = "Искандер", SurName = "Гизетдинов", Post = "Менеджер", Age = 30, Experience = "10 лет", Salary = 50000 });
+                    pharm.Add(new Pharmacist() { ID = 05, FirstName = "Данил", SurName = "Левачков", Post = "Уборщик", Age = 23, Experience = "23 года", Salary = 1000 });
 
                     Console.WriteLine();
+                    Console.WriteLine("Товары");
                     foreach (Product p in prod)
                     {
                         Console.WriteLine($"ID товара: {p.ID}", p.ID);
@@ -37,6 +45,19 @@ namespace StoreApp1
                         Console.WriteLine($"Производитель товара: {p.Maker}", p.Maker);
                         Console.WriteLine($"Доступность товара: {p.Availaible}", p.Availaible);
                         Console.WriteLine($"Кол-во товара: {p.Count}", p.Count);
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Фармацевты");
+                    foreach (Pharmacist p in pharm)
+                    {
+                        Console.WriteLine($"ID фармацевт: {p.ID}", p.ID);
+                        Console.WriteLine($"Имя: {p.FirstName}", p.FirstName);
+                        Console.WriteLine($"Фамилия: {p.SurName}", p.SurName);
+                        Console.WriteLine($"Должность: {p.Post}", p.Post);
+                        Console.WriteLine($"Возраст: {p.Age}", p.Age);
+                        Console.WriteLine($"Стаж: {p.Experience}", p.Experience);
+                        Console.WriteLine($"Зарплата: {p.Salary}", p.Salary);
                         Console.WriteLine();
                     }
                     goto case "2";
